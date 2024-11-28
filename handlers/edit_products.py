a01649d5580ff540558d131520cfbfa452d4b9b1
Bishkek25 <kunduz.qw8@gmail.com>
2024-11-28 23:30:54 +0600

    dz7

--- handlers/edit_products.py
+++
@@ -1,7 +0,0 @@
-# from aiogram import types, Dispatcher
-# from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-# from aiogram.dispatcher.filters import text
-# from aiogram.dispatcher import FSMContext
-# from aiogram.dispatcher.filters.state import state, StatesGroup
-
-
